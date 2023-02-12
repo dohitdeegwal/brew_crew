274c21ba7caba396cfc33aae7999e999046b0191
dohitdeegwal <76277872+dohitdeegwal@users.noreply.github.com>
2023-02-13 00:07:16 +0530

    Added firebase to whole app on all platforms

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,15 @@ import 'package:brew_crew/screens/wrapper.dart';
 import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
